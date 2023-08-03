eb0bfaa3a13a84034db514b5e1155f8a1b0d03f4
tomo <tomo9634@gmail.com>
2023-08-03 17:00:10 +1200

    typesファイルの修正

--- app/types/types.ts
+++ app/types/types.ts
@@ -1,10 +1,10 @@
 // 登録情報の型
 export type UserData = {
     user: {
-        "id": 0,
-        "name": "string",
-        "email": "string",
-        "iconImageUrl": "string"
+        id: number,
+        name: string,
+        email: string,
+        iconImageUrl: string
     },
-    token: "string"
+    token: string
 }
