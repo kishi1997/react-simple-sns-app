ccb1667fd9863140350a1c0152451cdea06aecde
tomo <tomo9634@gmail.com>
2023-09-15 12:15:00 +1200

    相手ユーザーのユーザー名とアイコン表示

--- app/models/rooms_model.ts
+++ app/models/rooms_model.ts
@@ -1,11 +1,16 @@
 import { roomsRepository } from "../repositories/rooms_repository";
 import { chatData } from "../types/chatData";
+import { roomUser } from "../types/response/responseRoomData";
 
 export const roomsFactory = () => {
     const repository = roomsRepository;
     return {
         getChatList: async():Promise<chatData[]> => {
             const response = await repository.getChatList();
+            return response;
+        },
+        getRoomData: async(params:string):Promise<roomUser[]> => {
+            const response = await repository.getRoomData(params);
             return response;
         }
     }
--- app/repositories/rooms_repository.ts
+++ app/repositories/rooms_repository.ts
@@ -1,15 +1,22 @@
 import { apiRequest } from "../axios/axiosInstance";
 import { chatData } from "../types/chatData";
+import { roomUser } from "../types/response/responseRoomData";
 
 export type RoomsRepository = {
  getChatList:() => Promise<chatData[]>;
+ getRoomData:(params: string) => Promise<roomUser[]>;
 }
 
 const getChatList:RoomsRepository["getChatList"] = async():Promise<chatData[]> => {
     const response = await apiRequest.get('/rooms');
     return response.data.rooms;
 }
+const getRoomData:RoomsRepository["getRoomData"] = async(params: string):Promise<roomUser[]> => {
+    const response = await apiRequest.get(`/rooms/${params}`);
+    return response.data.room.roomUsers;
+}
 
 export const roomsRepository: RoomsRepository = {
-    getChatList
+    getChatList,
+    getRoomData
 }
