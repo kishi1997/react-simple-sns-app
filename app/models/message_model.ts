ca31e8831d1e082f7c6860e56ec3af84a73bfa60
tomo <tomo9634@gmail.com>
2023-09-15 19:13:15 +1200

    message_model.ts内の変数名paramsを修正

--- app/models/message_model.ts
+++ app/models/message_model.ts
@@ -1,4 +1,3 @@
-import { promises } from "dns";
 import { messageRepository } from "../repositories/message_repository";
 import { commentDataParams } from "../types/params/commentDataParams";
 import { chatRoomData } from "../types/chatRoomData";
@@ -6,8 +5,8 @@ import { chatRoomData } from "../types/chatRoomData";
 export const messageFactory = () => {
     const repository = messageRepository;
     return {
-        addComment: async(params: commentDataParams) => {
-            await repository.addComment(params);
+        addComment: async(commentData: commentDataParams) => {
+            await repository.addComment(commentData);
         },
         getChat: async(roomid: string):Promise<chatRoomData[]> => {
             const response = await repository.getChat(roomid);
