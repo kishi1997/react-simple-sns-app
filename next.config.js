1e7b41edfbf086eac4d5809436fff29332e0cdc8
tomo <tomo9634@gmail.com>
2023-09-20 21:53:46 +1200

    外部URLの画像表示、再度修正

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const nextConfig = {}
 module.exports = {
     images: {
         domains: ['localhost'],
-        domains: ['simple-sns-front-1j630ttp7-c7mero.vercel.app']
+        domains: ['storage.googleapis.com']
     },
 };
