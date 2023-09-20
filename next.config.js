29b58f6c9bb8d7504b5808103fdf64e5d4643d9d
tomo <tomo9634@gmail.com>
2023-09-20 21:46:03 +1200

    外部URlの画像が表示されないため修正

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {}
 
 module.exports = {
     images: {
-        domains: ['localhost']
+        domains: ['localhost'],
+        domains: ['simple-sns-front-1j630ttp7-c7mero.vercel.app']
     },
 };
